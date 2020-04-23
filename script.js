3f8eaaac7d7f0b8d7ecf9c1faf59a6f976991b97
Bojo <bozhidar_zahariev@hotmail.com>
2020-04-23 10:14:36 +0100

    branch

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-//var randomColor1 = Math.floor(Math.random() * 16777215).toString(16);
-
-/*
-function getRandomColor() {
-  var letters = "0123456789ABCDEF".split("");
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.round(Math.random() * 15)];
-  }
-  return color;
-}
-*/
-
 //hex
 var randomColor = (Math.random().toString(16) + '000000').slice(2, 8);
 
@@ -54,7 +41,7 @@ const colorHex = () => {
   return hexArray;
 };
 
-const shuffleColors = a => {
+const shuffleColors = (a) => {
   for (let i = a.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [a[i], a[j]] = [a[j], a[i]];
@@ -74,26 +61,3 @@ display3.style.backgroundColor = `#${shuffledHex2.join('')}`;
 console.log(shuffledHex2[0].toUpperCase());
 console.log(shuffledHex2[1].toUpperCase());
 console.log(shuffledHex2[2].toUpperCase());
-
-/*
-//display rgb
-var display2 = document.getElementById('display2');
-var currentRGB = colorRGB();
-display2.textContent = currentRGB;
-display2.style.backgroundColor = 'rgb(' + currentRGB[0] + ',' + currentRGB[1] + ',' + currentRGB[2] + ')';
-
-for (let i = 0; i < 3; i++) {
-  let item = document.createElement('p');
-  item.classList.add('item');
-  display2.appendChild(item);
-}
-
-let child2 = display2.getElementsByClassName('item');
-child2[0].textContent = currentRGB[0];
-//needs checking the color
-child2[0].style.backgroundColor = 'rgb(' + currentRGB[0] + ',' + 0 + ',' + 0 + ')';
-child2[1].textContent = currentRGB[1];
-child2[1].style.backgroundColor = 'rgb(' + 0 + ',' + currentRGB[1] + ',' + 0 + ')';
-child2[2].textContent = currentRGB[2];
-child2[2].style.backgroundColor = 'rgb(' + 0 + ',' + 0 + ',' + currentRGB[2] + ')';
-*/
